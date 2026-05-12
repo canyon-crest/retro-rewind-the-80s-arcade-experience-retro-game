3fbcf0d87c9ec40a037d5347d171ea8a496d2f2b
Siddh-J <jariwalas9581@my.sduhsd.net>
2026-05-12 22:26:38 +0000

    day 1

--- sketch.js
+++ sketch.js
@@ -17,9 +17,26 @@ groundB.width = 220;
 groundB.rotation = -30;
 groundB.physics = STATIC;
 
+let wall = new Sprite();
+wall.x = 200;
+wall.y = -100
+wall.width = 220;
+wall.rotation = 90;
+wall.physics = STATIC;
+
+let wall2 = new Sprite();
+wall2.x = -200;
+wall2.y = -100
+wall2.width = 220;
+wall2.rotation = 90;
+wall2.physics = STATIC;
+
 q5.update = function () {
 	background('skyblue');
 	text('click to jump!', 0, -50);
 
-	if (mouse.presses()) ball.vel.y = -5;
+	if (kb.pressing('w')) ball.vel.y = -5;
+	if (kb.pressing('d')) ball.vel.x = 5
+	if (kb.pressing('a')) ball.vel.x = -5
+	if (kb.pressing('m')) ball.scale = min(ball.scale + 0.01, 2);
 };
